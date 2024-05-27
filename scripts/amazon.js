1b40127ea877baf37aa930502a30cdd30ac2c9c3
Laurent Tshisekedi <Laurent.N.Tshisekedi@gmail.com>
2024-05-26 19:52:29 -0500

    move addToCart to cart.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -58,36 +58,42 @@ products.forEach((product) => {
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML; 
 
+function addToCart(productId) {
+    let matchingItem; 
+
+    cart.forEach((cartItem) => {
+        if (productId === cartItem.productId) {
+            matchingItem = cartItem; 
+        }
+    } )
+
+    if (matchingItem) {
+        matchingItem.quantity += 1
+    } else {
+        cart.push({
+            productId: productId, 
+            quantity: 1
+        })
+    }}
+
+function updateCartQuantity(){
+
+    let cartQuantity = 0 
+
+    cart.forEach((cartItem) => {
+         cartQuantity += cartItem.quantity
+    })
+
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity; 
+}    
+
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     button.addEventListener('click', () => {
         const productId = button.dataset.productId; 
 
-        let matchingItem; 
-
-        cart.forEach((item) => {
-            if (productId === item.productId) {
-                matchingItem = item; 
-            }
-        } )
-
-        if (matchingItem) {
-            matchingItem.quantity += 1
-        } else {
-            cart.push({
-                productId: productId, 
-                quantity: 1
-            })
-        }
-
-        let cartQuantity = 0 
-
-        cart.forEach((item) => {
-             cartQuantity += item.quantity
-        })
-
-        document.querySelector('.js-cart-quantity').innerHTML = cartQuantity; 
+        addToCart(productId); 
+        updateCartQuantity(); 
     }); 
-    
 }); 
 
 
